343b8041a789c8fc1335048e1b5e9c31c5f1731e
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-03-15 21:50:23 -0600

    code function correctly

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from FSM import FiniteStateMachine
 
+def isEven(fsm, initState, finalState, string):
+    currentState = initState
+    for symbol in string:
+       nextState = AFD.getState(currentState, symbol)
+       currentState = nextState
+    acceptedState = currentState
+    if acceptedState == finalState:
+        return "SI es aceptada"
+    else:
+        return "NO es aceptada"
+
 if __name__ == "__main__":
+
     AFD = FiniteStateMachine()
     AFD.setTransition("q0", "1", "q1")
     AFD.setTransition("q0", "0", "q3")
@@ -14,23 +26,12 @@ if __name__ == "__main__":
     AFD.setTransition("q3", "1", "q2")
     AFD.setTransition("q3", "0", "q0")
 
-    initState = "q0"
-    currentState = "q0"
-    acceptedState = ""
-    prevState = ""
-    nextState = ""
-    string = ""
+    print("0011 " + isEven(AFD, "q0", "q0", "0011"))
+    print("00 " + isEven(AFD, "q0", "q0", "00"))
+    print("001 " + isEven(AFD, "q0", "q0", "001"))
+    print("00011 " + isEven(AFD, "q0", "q0", "00011"))
+    print("000111 " + isEven(AFD, "q0", "q0", "000111"))
+    print("" + isEven(AFD, "q0", "q0", ""))
     
-    for i in string:
-       nextState = AFD.getState(initState, i)
-       initState = nextState
-
-    acceptedState = initState
-    if acceptedState == "q0":
-        print("Aceptado")
-    else:
-        print("Rechazado")
-
-    #print(AFD.delta)
 
 
