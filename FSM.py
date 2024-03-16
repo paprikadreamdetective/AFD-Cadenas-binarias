1f78743b1285656f9a6be8a183b9fa4fbeea4213
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2024-03-15 21:49:52 -0600

    detected even parity is ok

--- FSM.py
+++ FSM.py
@@ -1,6 +1,6 @@
 class FiniteStateMachine():
     def __init__(self):
-        self.delta = {():''}
+        self.delta = {}
     
     def setTransition(self, currentState, input, nextSate):
         self.delta.update({(currentState, input) : nextSate})
--- main.py
+++ main.py
@@ -2,15 +2,35 @@ from FSM import FiniteStateMachine
 
 if __name__ == "__main__":
     AFD = FiniteStateMachine()
-    AFD.setTransition("q0", 1, "q1")
-    AFD.setTransition("q0", 0, "q3")
-    AFD.setTransition("q1", 1, "q0")
-    AFD.setTransition("q1", 0, "q2")
-    AFD.setTransition("q2", 1, "q3")
-    AFD.setTransition("q2", 0, "q1")
-    AFD.setTransition("q3", 1, "q2")
-    AFD.setTransition("q3", 0, "q0")
+    AFD.setTransition("q0", "1", "q1")
+    AFD.setTransition("q0", "0", "q3")
 
+    AFD.setTransition("q1", "1", "q0")
+    AFD.setTransition("q1", "0", "q2")
 
+    AFD.setTransition("q2", "1", "q3")
+    AFD.setTransition("q2", "0", "q1")
+
+    AFD.setTransition("q3", "1", "q2")
+    AFD.setTransition("q3", "0", "q0")
+
+    initState = "q0"
+    currentState = "q0"
+    acceptedState = ""
+    prevState = ""
+    nextState = ""
+    string = ""
     
+    for i in string:
+       nextState = AFD.getState(initState, i)
+       initState = nextState
+
+    acceptedState = initState
+    if acceptedState == "q0":
+        print("Aceptado")
+    else:
+        print("Rechazado")
+
+    #print(AFD.delta)
+
 
